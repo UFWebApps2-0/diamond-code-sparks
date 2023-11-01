3baa770fdbd7e237143540bcb19c551d5d3ca4a0
emeckley <emeckley@ufl.edu>
2023-11-01 14:04:26 -0400

    Created sample org card

--- client/src/views/OrgDashboard/OrgDashboard.jsx
+++ client/src/views/OrgDashboard/OrgDashboard.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
-import Logo from "../../assets/casmm_logo.png";
+import React, { useEffect, useState } from 'react';
 import NavBar from "../../components/NavBar/NavBar";
 import './OrgDashboard.less'
 import { useNavigate } from 'react-router-dom';
 
 export default function OrgDashboard() {
+	const [orgs, setOrgs] = useState([]);
 	const navigate = useNavigate();
+
+	const sampleOrg = {
+		id: 1,
+		name: "Org 1",
+		school: {
+			name: "School Name"
+		},
+		classrooms: [
+			{id:1, name:"Class 1"},
+			{id:2, name:"Class 2"}
+		],
+		teachers: [
+			{id:1, name:"Teacher 1"}
+		],
+		students: [
+			{id:1, name:"Student 1"},
+			{id:2, name:"Student 2"},
+			{id:3, name:"Student 3"}
+		]
+	}
+
+	orgs.push(sampleOrg);
     
 	return (
 	    <div className='container nav-padding'>
@@ -16,7 +38,37 @@ export default function OrgDashboard() {
 	        	<h1>Your Organizations</h1>
 	        </div>
 	        <div id='orgs-container'>
-	        	<p>[Orgs will go here]</p>
+	        	<div id='dashboard-card-container'>
+	        		{orgs.map((org) => (
+		        		<div key={org.id} id='dashboard-org-card'>
+		        			<div id='card-left-content-container'>
+		        				<h1 id='card-title'>{org.name}</h1>
+		        				<p>{org.school.name}</p>
+		        				<div id='card-button-container' className='flex flex-row'>
+		        					<button onClick={() => alert("view")}>
+		        						View
+		        					</button>
+		        				</div>
+		        			</div>
+		        			<div id='card-right-content-container'>
+		        				<div id='number-container'>
+		        					<h1 id='number'>{org.classrooms.length}</h1>
+		        					<p id='label'>Classrooms</p>
+		        				</div>
+		        				<div id='divider' />
+		        				<div id='number-container'>
+		        					<h1 id='number'>{org.teachers.length}</h1>
+		        					<p id='label'>Teachers</p>
+		        				</div>
+		        				<div id='divider' />
+		        				<div id='number-container'>
+		        					<h1 id='number'>{org.students.length}</h1>
+		        					<p id='label'>Students</p>
+		        				</div>
+		        			</div>
+		        		</div>
+		        	))}
+	        	</div>
 	        	<input
 	        		type='button'
 	        		onClick={() => navigate('/createorg')}
