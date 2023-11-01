beeeeaa24c4bfd818efbdcbcc91dc36acf4e067a
KKonuru <kausthubhkonuru@gmail.com>
2023-11-01 14:05:12 -0400

    Create Assessment File

---
+++ client/src/views/Assessments/Assessments.jsx
@@ -0,0 +1,16 @@
+import NavBar from '../../components/NavBar/NavBar';
+
+
+function Assessments()
+{
+
+    return (
+
+        <div>
+            <NavBar />
+            <p>Hello World!!</p>
+        </div>
+    )
+}
+
+export default Assessments;
