19adb1f9a64a936f2dad0abf302509e6b9f6ca88
Julian <stennett.j@ufl.edu>
2023-11-04 01:37:36 -0400

    created main dashboard/frontfacing view of the app

--- client/src/views/Student/Student.jsx
+++ client/src/views/Student/Student.jsx
@@ -1,4 +1,4 @@
-import { message } from 'antd';
+import { Button, message } from 'antd';
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import NavBar from '../../components/NavBar/NavBar';
@@ -7,6 +7,15 @@ import './Student.less';
 
 function Student() {
   const [learningStandard, setLessonModule] = useState({});
+  const [view_activity, setActivity] = useState(false);
+  const [view_class, setClass] = useState(false);
+  const [view_other_lesson, setLesson] = useState(false);
+  const [view_make_programs, setMakeProgram] = useState(false);
+  const [view_my_programs, setPrograms] = useState(false);
+  const [view_share, setShare] = useState(false);
+  const [view_sharedFrom, setSharedFrom] = useState(false);
+  const [view_Gallery, setGallery] = useState(false);
+  const [view_menu, setMenu] = useState(true);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -25,6 +34,51 @@ function Student() {
     fetchData();
   }, []);
 
+  function viewActivities(status) {
+    setActivity(status);
+    setMenu(false);
+  }
+
+  function viewClass(status) {
+    setClass(status);
+    setMenu(false);
+  }
+
+  function viewOtherLesson(status) {
+    setLesson(status);
+    setMenu(false);
+  }
+
+  function viewMProgram(status) {
+    setMakeProgram(status);
+    setMenu(false);
+  }
+
+  function viewMProgram(status) {
+    setMakeProgram(status);
+    setMenu(false);
+  }
+
+  function viewProgram(status) {
+    setPrograms(status);
+    setMenu(false);
+  }
+
+  function viewShare(status) {
+    setShare(status);
+    setMenu(false);
+  }
+
+  function viewSharedF(status) {
+    setSharedFrom(status);
+    setMenu(false);
+  }
+
+  function viewGallery(status) {
+    setGallery(status);
+    setMenu(false);
+  }
+
   const handleSelection = (activity) => {
     activity.lesson_module_name = learningStandard.name;
     localStorage.setItem('my-activity', JSON.stringify(activity));
@@ -35,6 +89,64 @@ function Student() {
   return (
     <div className='container nav-padding'>
       <NavBar />
+      {view_menu && (
+      <div>
+        <div className='Welcome'>
+          <h1 className="Hello">Hello, StudentName!</h1>
+        </div>
+        <div className='dbSection'>
+          <div className="section_label">
+            Class view
+          </div>
+          <div className="line"></div>
+        </div>
+        <div className="buttons1">
+          <button id="DailyActivity" onClick={() => viewActivities(true)}>
+            <i className='fa fa-calendar-check' style={{ marginRight: '10px' }}/>
+            Daily Activity
+          </button>
+          <button id="Classroom" onClick={() => viewClass(true)}>
+            <i className='fa fa-book' style={{ marginRight: '10px' }}/>
+            Classroom
+          </button>
+          <button id="OtherLessons" onClick={() => viewOtherLesson(true)}>
+            <i className='fa fa-bars' style={{ marginRight: '10px' }}/>
+            Other Lessons
+          </button>
+        </div>
+        <div className='dbSection'>
+          <div className="section_label">
+            Programs
+          </div>
+          <div className="line"></div>
+        </div>
+        <div className="buttons1">
+          <button id="MakeNewProgram" onClick={() => viewMProgram(true)}>
+            <i className='fas fa-plus' style={{ marginRight: '10px' }}/>
+            Make New Program
+          </button>
+          <button id="MyPrograms" onClick={() => viewProgram(true)}>
+            <i className="fa fa-layer-group" style={{ marginRight: '10px' }}/>
+            My Programs
+          </button>
+        </div>
+        <div className='buttons1'>
+          <button id="shareProgram" onClick={() => viewShare(true)}>
+            <i className='fa fa-paper-plane' style={{ marginRight: '10px' }}/>
+            Share Program
+          </button>
+          <button id="sharedWithYou" onClick={() => viewSharedF(true)}>
+            <i className='fa fa-envelope' style={{ marginRight: '10px' }}/>
+            Shared With You
+          </button>
+          <button id="viewGallery" onClick={() => viewGallery(true)}>
+            <i className='fa fa-globe' style={{ marginRight: '10px' }}/>
+            View Gallery
+          </button>
+        </div>
+      </div>
+    )}
+      {view_activity && (
       <div id='activity-container'>
         <div id='header'>
           <div>Select your Activity</div>
@@ -62,6 +174,63 @@ function Student() {
           )}
         </ul>
       </div>
+      )}
+      {/*add view class page*/}
+      {view_class && (
+      <div id='activity-container'>
+        <div id='header'>
+          <div>View Class</div>
+        </div>
+      </div>
+      )}
+      {/*add other lesson page*/}
+      {view_other_lesson && (
+      <div id='activity-container'>
+        <div id='header'>
+          <div>Other Lessons</div>
+        </div>
+      </div>
+      )}
+      {/*add other lesson page*/}
+      {view_make_programs && (
+      <div id='activity-container'>
+        <div id='header'>
+          <div>Make Program</div>
+        </div>
+      </div>
+      )}
+      {/*add My Programs Page*/}
+      {view_my_programs && (
+      <div id='activity-container'>
+        <div id='header'>
+          <div>My Programs</div>
+        </div>
+      </div>
+      )}
+      {/*add share page*/}
+      {view_share && (
+      <div id='activity-container'>
+        <div id='header'>
+          <div>Share Program</div>
+        </div>
+      </div>
+      )}
+      {/*add shared with you page*/}
+      {view_sharedFrom && (
+      <div id='activity-container'>
+        <div id='header'>
+          <div>Shared Programs</div>
+        </div>
+      </div>
+      )}
+      {/* add View Gallery */}
+      {view_Gallery && (
+      <div id='activity-container'>
+        <div id='header'>
+          <div>Gallery</div>
+        </div>
+      </div>
+      )}
     </div>
   );
 }
