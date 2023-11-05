a3fa97b21f43d1a99fde2dd59022cc6a5a4ff6c8
Ash Savigne <ashleysavigne@gmail.com>
2023-11-04 23:10:38 -0400

    Update PublicCanvas.jsx

--- client/src/components/ActivityPanels/BlocklyCanvasPanel/canvas/PublicCanvas.jsx
+++ client/src/components/ActivityPanels/BlocklyCanvasPanel/canvas/PublicCanvas.jsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useRef, useState, useReducer } from 'react';
+import { Link } from 'react-router-dom';
 import '../../ActivityLevels.less';
+// Ashley Savigne: added handle save
 import { compileArduinoCode, handleSave } from '../../Utils/helpers';
-import { message, Spin, Row, Col, Alert, Dropdown, Menu } from 'antd';
+import { message, Spin, Row, Col, Alert, Menu, Dropdown } from 'antd';
+// Ashley Savigne: added getsaves
 import { getSaves } from '../../../../Utils/requests';
 import CodeModal from '../modals/CodeModal';
 import ConsoleModal from '../modals/ConsoleModal';
 import PlotterModal from '../modals/PlotterModal';
+// Ashley Savigne: added displayDiagram and verson hist
 import DisplayDiagramModal from '../modals/DisplayDiagramModal'
 import VersionHistoryModal from '../modals/VersionHistoryModal';
 import {
@@ -15,16 +19,15 @@ import {
 } from '../../Utils/consoleHelpers';
 import ArduinoLogo from '../Icons/ArduinoLogo';
 import PlotterLogo from '../Icons/PlotterLogo';
+// ashley: added usenav
 import { useNavigate } from 'react-router-dom';
 
 let plotId = 1;
 
-export default function StudentCanvas({ activity }) {
-  const [hoverSave, setHoverSave] = useState(false);
+export default function PublicCanvas({ activity, isSandbox }) {
   const [hoverUndo, setHoverUndo] = useState(false);
   const [hoverRedo, setHoverRedo] = useState(false);
   const [hoverCompile, setHoverCompile] = useState(false);
-  const [hoverImage, setHoverImage] = useState(false);
   const [hoverConsole, setHoverConsole] = useState(false);
   const [showConsole, setShowConsole] = useState(false);
   const [showPlotter, setShowPlotter] = useState(false);
@@ -32,17 +35,10 @@ export default function StudentCanvas({ activity }) {
   const [connectionOpen, setConnectionOpen] = useState(false);
   const [selectedCompile, setSelectedCompile] = useState(false);
   const [compileError, setCompileError] = useState('');
-  const [saves, setSaves] = useState({});
-  const [lastSavedTime, setLastSavedTime] = useState(null);
-  const [lastAutoSave, setLastAutoSave] = useState(null);
 
   const [forceUpdate] = useReducer((x) => x + 1, 0);
-  const navigate = useNavigate();
   const workspaceRef = useRef(null);
   const activityRef = useRef(null);
-
-  const replayRef = useRef([]);
-  const clicks = useRef(0);
 
   const setWorkspace = () => {
     workspaceRef.current = window.Blockly.inject('blockly-canvas', {
@@ -51,194 +47,25 @@ export default function StudentCanvas({ activity }) {
     window.Blockly.addChangeListener(blocklyEvent);
   };
 
-
-
-  const loadSave = (selectedSave) => {
-    try {
-      let toLoad = activity.template;
-      if (selectedSave !== -1) {
-        if (lastAutoSave && selectedSave === -2) {
-          toLoad = lastAutoSave.workspace;
-          setLastSavedTime(getFormattedDate(lastAutoSave.updated_at));
-        } else if (saves.current && saves.current.id === selectedSave) {
-          toLoad = saves.current.workspace;
-          setLastSavedTime(getFormattedDate(saves.current.updated_at));
-        } else {
-          const s = saves.past.find((save) => save.id === selectedSave);
-          if (s) {
-            toLoad = s.workspace;
-            setLastSavedTime(getFormattedDate(s.updated_at));
-          } else {
-            message.error('Failed to restore save.');
-            return;
-          }
-        }
-      } else {
-        setLastSavedTime(null);
-      }
-      let xml = window.Blockly.Xml.textToDom(toLoad);
-      if (workspaceRef.current) workspaceRef.current.clear();
-      window.Blockly.Xml.domToWorkspace(xml, workspaceRef.current);
-      workspaceRef.current.clearUndo();
-    } catch (e) {
-      message.error('Failed to load save.');
-    }
-  };
-
-  const pushEvent = (type, blockId = '') => {
-    let blockType = '';
-    if (blockId !== '') {
-      let type = window.Blockly.mainWorkspace.getBlockById(blockId)?.type;
-      type ? blockType = type : blockType = ''; 
-    }
-
-    let xml = window.Blockly.Xml.workspaceToDom(workspaceRef.current);
-    let xml_text = window.Blockly.Xml.domToText(xml);
-    replayRef.current.push({
-      xml: xml_text,
-      action: type,
-      blockId: blockId,
-      blockType: blockType,
-      timestamp: Date.now(),
-      clicks: clicks.current,
-    });
-  };
-
-  let blocked = false;
-  const blocklyEvent = (event) => {
-    // if it is a click event, add click
-    if (
-      (event.type === 'ui' && event.element === 'click') ||
-      event.element === 'selected'
-    ) {
-      clicks.current++;
-    }
-
-    // if it is other ui events or create events or is [undo, redo], return
-    if (event.type === 'ui' || !event.recordUndo) {
-      return;
-    }
-
-    // if event is in timeout, return
-    if (event.type === 'change' && blocked) {
-      return;
-    }
-
-    // if the event is change field value, only accept the latest change
-    if (
-      event.type === 'change' &&
-      event.element === 'field' &&
-      replayRef.current.length > 1 &&
-      replayRef.current[replayRef.current.length - 1].action ===
-        'change field' &&
-      replayRef.current[replayRef.current.length - 1].blockId === event.blockId
-    ) {
-      replayRef.current.pop();
-    }
-
-    // event delete always comes after a move, ignore the move
-    if (event.type === 'delete') {
-      if (replayRef.current[replayRef.current.length - 1].action === 'move') {
-        replayRef.current.pop();
-      }
-    }
-
-    // if event is change, add the detail action type
-    if (event.type === 'change' && event.element) {
-      pushEvent(`${event.type} ${event.element}`, event.blockId);
-    } else {
-      pushEvent(event.type, event.blockId);
-    }
-
-    // timeout for half a second
-    blocked = true;
-    setTimeout(() => {
-      blocked = false;
-    }, 500);
-  };
-
-  useEffect(() => {
-    // automatically save workspace every min
-    let autosaveInterval = setInterval(async () => {
-      if (workspaceRef.current && activityRef.current) {
-        const res = await handleSave(
-          activityRef.current.id,
-          workspaceRef,
-          replayRef.current
-        );
-        if (res.data) {
-          setLastAutoSave(res.data[0]);
-          setLastSavedTime(getFormattedDate(res.data[0].updated_at));
-        }
-      }
-    }, 60000);
-
-    // clean up - saves workspace and removes blockly div from DOM
-    return async () => {
-      clearInterval(autosaveInterval);
-    };
-  }, []);
-
   useEffect(() => {
     // once the activity state is set, set the workspace and save
     const setUp = async () => {
       activityRef.current = activity;
       if (!workspaceRef.current && activity && Object.keys(activity).length !== 0) {
         setWorkspace();
-
-        let onLoadSave = null;
-        const res = await getSaves(activity.id);
-        if (res.data) {
-          if (res.data.current) onLoadSave = res.data.current;
-          setSaves(res.data);
-        } else {
-          console.log(res.err);
-        }
-
-        if (onLoadSave) {
-          let xml = window.Blockly.Xml.textToDom(onLoadSave.workspace);
-          window.Blockly.Xml.domToWorkspace(xml, workspaceRef.current);
-          replayRef.current = onLoadSave.replay;
-          setLastSavedTime(getFormattedDate(onLoadSave.updated_at));
-        } else if (activity.template) {
-          let xml = window.Blockly.Xml.textToDom(activity.template);
-          window.Blockly.Xml.domToWorkspace(xml, workspaceRef.current);
-        }
-
-        pushEvent('load workspace');
-        workspaceRef.current.clearUndo();
       }
     };
     setUp();
   }, [activity]);
 
-  const handleManualSave = async () => {
-    // save workspace then update load save options
-    pushEvent('save');
-    const res = await handleSave(activity.id, workspaceRef, replayRef.current);
-    if (res.err) {
-      message.error(res.err);
-    } else {
-      setLastSavedTime(getFormattedDate(res.data[0].updated_at));
-      message.success('Workspace saved successfully.');
-    }
-
-    const savesRes = await getSaves(activity.id);
-    if (savesRes.data) setSaves(savesRes.data);
-  };
-
   const handleUndo = () => {
-    if (workspaceRef.current.undoStack_.length > 0) {
+    if (workspaceRef.current.undoStack_.length > 0)
       workspaceRef.current.undo(false);
-      pushEvent('undo');
-    }
   };
 
   const handleRedo = () => {
-    if (workspaceRef.current.redoStack_.length > 0) {
+    if (workspaceRef.current.redoStack_.length > 0)
       workspaceRef.current.undo(true);
-      pushEvent('redo');
-    }
   };
 
   const handleConsole = async () => {
@@ -257,7 +84,6 @@ export default function StudentCanvas({ activity }) {
       }
       setConnectionOpen(true);
       setShowConsole(true);
-      pushEvent('show serial monitor');
     }
     // if serial monitor is shown, close the connection
     else {
@@ -290,7 +116,6 @@ export default function StudentCanvas({ activity }) {
       }
       setConnectionOpen(true);
       setShowPlotter(true);
-      pushEvent('show serial plotter');
     } else {
       plotId = 1;
       if (connectionOpen) {
@@ -300,6 +125,7 @@ export default function StudentCanvas({ activity }) {
       setShowPlotter(false);
     }
   };
+
   const handleCompile = async () => {
     if (showConsole || showPlotter) {
       message.warning(
@@ -319,24 +145,9 @@ export default function StudentCanvas({ activity }) {
         setSelectedCompile,
         setCompileError,
         activity,
-        true
+        false
       );
-      pushEvent('compile');
     }
-  };
-
-  const handleGoBack = () => {
-    if (
-      window.confirm(
-        'All unsaved progress will be lost. Do you still want to go back?'
-      )
-    )
-      navigate(-1);
-  };
-
-  const getFormattedDate = (value, locale = 'en-US') => {
-    let output = new Date(value).toLocaleDateString(locale);
-    return output + ' ' + new Date(value).toLocaleTimeString(locale);
   };
 
   const menu = (
@@ -345,6 +156,7 @@ export default function StudentCanvas({ activity }) {
         <PlotterLogo />
         &nbsp; Show Serial Plotter
       </Menu.Item>
+      <CodeModal title={'XML'} workspaceRef={workspaceRef.current} />
       <Menu.Item>
         <CodeModal title={'Arduino Code'} workspaceRef={workspaceRef.current} />
       </Menu.Item>
@@ -366,53 +178,24 @@ export default function StudentCanvas({ activity }) {
           >
             <Row id='icon-control-panel'>
               <Col flex='none' id='section-header'>
-                {activity.lesson_module_name}
+                Program your Arduino...
               </Col>
               <Col flex='auto'>
                 <Row align='middle' justify='end' id='description-container'>
                   <Col flex={'30px'}>
-                    <button
-                      onClick={handleGoBack}
-                      id='link'
-                      className='flex flex-column'
-                    >
-                      <i id='icon-btn' className='fa fa-arrow-left' />
-                    </button>
+                    <Row>
+                      <Col>
+                        <Link id='link' to={'/'} className='flex flex-column'>
+                          <i className='fa fa-home fa-lg' />
+                        </Link>
+                      </Col>
+                    </Row>
                   </Col>
                   <Col flex='auto' />
 
-                  <Col flex={'300px'}>
-                    {lastSavedTime ? `Last changes saved ${lastSavedTime}` : ''}
-                  </Col>
-                  <Col flex={'350px'}>
+                  <Col flex={'200px'}>
                     <Row>
-                      <Col className='flex flex-row' id='icon-align'>
-                        <VersionHistoryModal
-                          saves={saves}
-                          lastAutoSave={lastAutoSave}
-                          defaultTemplate={activity}
-                          getFormattedDate={getFormattedDate}
-                          loadSave={loadSave}
-                          pushEvent={pushEvent}
-                        />
-                        <button
-                          onClick={handleManualSave}
-                          id='link'
-                          className='flex flex-column'
-                        >
-                          <i
-                            id='icon-btn'
-                            className='fa fa-save'
-                            onMouseEnter={() => setHoverSave(true)}
-                            onMouseLeave={() => setHoverSave(false)}
-                          />
-                          {hoverSave && (
-                            <div className='popup ModalCompile4'>Save</div>
-                          )}
-                        </button>
-                      </Col>
-
-                      <Col className='flex flex-row' id='icon-align'>
+                      <Col className='flex flex-row'>
                         <button
                           onClick={handleUndo}
                           id='link'
@@ -460,7 +243,7 @@ export default function StudentCanvas({ activity }) {
                       </Col>
                     </Row>
                   </Col>
-                  <Col flex={'180px'}>
+                  <Col flex={'230px'}>
                     <div
                       id='action-btn-container'
                       className='flex space-around'
@@ -474,9 +257,7 @@ export default function StudentCanvas({ activity }) {
                           Upload to Arduino
                         </div>
                       )}
-                    <DisplayDiagramModal
-                      image={activity.images}
-                    />
+
                       <i
                         onClick={() => handleConsole()}
                         className='fas fa-terminal hvr-info'
@@ -500,7 +281,6 @@ export default function StudentCanvas({ activity }) {
             <div id='blockly-canvas' />
           </Spin>
         </div>
-
         <ConsoleModal
           show={showConsole}
           connectionOpen={connectionOpen}
@@ -513,7 +293,7 @@ export default function StudentCanvas({ activity }) {
           plotData={plotData}
           setPlotData={setPlotData}
           plotId={plotId}
-        />          
+        />
       </div>
 
       {/* This xml is for the blocks' menu we will provide. Here are examples on how to include categories and subcategories */}
