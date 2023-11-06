453d4c9900f2c42f392552350d1acff60fa4aed2
KKonuru <kausthubhkonuru@gmail.com>
2023-11-05 19:37:34 -0500

    Connected assessments to backend
    Works with strapi now

--- client/src/views/Mentor/Classroom/Classroom.jsx
+++ client/src/views/Mentor/Classroom/Classroom.jsx
@@ -7,7 +7,6 @@ import Home from './Home/Home';
 import SavedWorkSpaceTab from '../../../components/Tabs/SavedWorkspaceTab';
 import Assessments from '../../Assessments/Assessments';
 import { useSearchParams, useParams } from 'react-router-dom';
-import { AssessmentButton } from '../../Assessments/AssessmentButton';
 const { TabPane } = Tabs;
 const name ="assessment1";
 const questionSet = {
@@ -67,6 +66,9 @@ export default function Classroom({
             setSearchParams={setSearchParams}
             classroomId={id}
           />
+        </TabPane>
+        <TabPane tab='Assessments' key='assessments'>
+          <Assessments classroomId={id} />
         </TabPane>
       </Tabs>
     </div>
