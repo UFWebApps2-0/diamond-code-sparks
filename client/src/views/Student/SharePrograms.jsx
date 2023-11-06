734369510c75230b9724dcf74a2e7766e26c24e2
Ruhi Reddy <111948952+ruhireddy@users.noreply.github.com>
2023-11-07 06:21:29 +0900

    select name function

--- client/src/views/Student/SharePrograms.jsx
+++ client/src/views/Student/SharePrograms.jsx
@@ -1,6 +1,34 @@
 import React, { useState } from 'react';
 import { Button, Modal } from 'antd';
+import { Select } from 'antd';
 function ShareProgram(){
+    const [selectedPerson, setSelectedPerson] = useState({});
+    function SelectPerson(){
+        const onChange = (value) => {
+            setSelectedPerson(value);
+        };
+        return(
+            <Select
+                showSearch
+                placeholder="Select a student"
+                onChange={onChange}
+                options={[
+                {
+                    value: 'jack',
+                    label: 'Jack',
+                },
+                {
+                    value: 'lucy',
+                    label: 'Lucy',
+                },
+                {
+                    value: 'tom',
+                    label: 'Tom',
+                },
+                ]}
+            />
+        );
+    }
     
     function ShareButton(){
         const [isModalOpen, setIsModalOpen] = useState(false);
@@ -57,6 +85,6 @@ function ShareProgram(){
 
     return (
         
-    )
+    );
 }
 export default ShareProgram;
