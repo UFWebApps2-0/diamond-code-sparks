8b0e0476dc362d4fe52b48a23828523cb0517f80
sethbehar <sethbehar@gmail.com>
2023-11-27 14:00:52 -0500

    Finished styling for student assessment view

--- client/src/views/StudentAssessment/TakeAssess.jsx
+++ client/src/views/StudentAssessment/TakeAssess.jsx
@@ -1,75 +1,76 @@
-// Import necessary components and styles
 import { Select, Radio, Space } from 'antd';
 import { Button, Form, Input } from 'antd';
 import { Card } from 'antd';
 import React, { useEffect, useState } from 'react';
-import NavBar from "../../components/NavBar/NavBar";
-
-// Basic CSS for styling
-const submitButtonStyle = {
-    marginTop: '16px',
-    marginBottom: '42px',
-    alignSelf: 'flex-end',
-    backgroundColor: 'white',
-    color: 'black',
-    width: '30%',
-    height: '30%',
-    fontSize: '1.2rem'
-};
+import NavBar from "../../components/NavBar/NavBar"
+import { getStudentClassroom, createStudentAssessments } from '../../Utils/requests';
 
 function TakeAssess() {
-   function SubmitAssessment() {
-    
-   }
+    const data = JSON.parse(localStorage.getItem('my-assessment'));
+    const [answers, setAnswers] = useState(new Array(data["questions"].length).fill(false));
 
-   const data = JSON.parse(localStorage.getItem('my-assessment'));
+    async function SubmitAssessment() {
+        for (let i = 0; i < answers.length; i++) {
+            if (answers[i] === false) {
+                alert("You must enter an answer for question " + (i + 1));
+                return;
+            }
+        }
+        let res = await getStudentClassroom();
+        const classID = res.data.classroom.id;
+        let id = JSON.parse(localStorage.getItem("studentsID"))[0];
+        console.log(classID);
+        createStudentAssessments(data["name"], id, classID, answers);
+    }
 
-   return (
-      <div className="container nav-padding">
-         <NavBar />
+    function onChange(index, e) {
+        let temp = [...answers];
+        temp[index] = e.target.value;
+        setAnswers(temp);
+        console.log(answers);
+    }
 
-         <Card title={<h1 style={{ fontSize: '2rem' }}>{data["name"]}</h1>} >
-               <h2>Description: {data["description"]}</h2>
-            <Space direction="vertical" size={32}>
-               {data["questions"].map((question, index) => (
-                  <div key={index}>
-                     {question.type === "Multiple Choice" ? (
-                        <div>
-                           <Card title={`Question ${index + 1}. ${question.question}`} >
-                              <Radio.Group>
-                                 <Space direction="vertical">
-                                    {question.option.map((option, optionIndex) => (
-                                       <Radio key={optionIndex} value={optionIndex}>
-                                          {option}
-                                       </Radio>
-                                    ))}
-                                 </Space>
-                              </Radio.Group>
-                           </Card>
+    return (
+        <div className="container nav-padding">
+            <NavBar />
+            <Card title={<h1 style={{ fontSize: '28px' }}>{data["name"]}</h1>} style={{ maxWidth: '800px', margin: '0 auto' }}>
+                <h2>Description: {data["description"]}</h2>
+                <Space direction="vertical" size={32}>
+                    {data["questions"].map((question, index) => (
+                        <div key={index}>
+                            {question.type === "Multiple Choice" ? (
+                                <Card title={`Question ${index + 1}. ${question.question}`} style={{ width: '100%' }}>
+                                    <Radio.Group onChange={(e) => onChange(index, e)}>
+                                        <Space direction="vertical">
+                                            {question.option.map((option, optionIndex) => (
+                                                <Radio value={optionIndex} key={optionIndex}>{option}</Radio>
+                                            ))}
+                                        </Space>
+                                    </Radio.Group>
+                                </Card>
+                            ) : (
+                                <Card title={`Question ${index + 1}. ${question.question}`} style={{ width: '100%' }}>
+                                    <Form id="activity-detail-editor" layout="horizontal" size="default" labelCol={{
+                                        span: 6,
+                                    }}
+                                        wrapperCol={{
+                                            span: 14,
+                                        }}>
+                                        <Form.Item labelAlign='left' onChange={(e) => onChange(index, e)}>
+                                            <Input.TextArea />
+                                        </Form.Item>
+                                    </Form>
+                                </Card>
+                            )}
                         </div>
-                     ) : (
-                        <div>
-                           <Card title={`Question ${index + 1}. ${question.question}`} >
-                              <Form id="activity-detail-editor" layout="horizontal" size="default" labelCol={{ span: 6 }} wrapperCol={{ span: 14 }}>
-                                 <Form.Item labelAlign='left'>
-                                    <Input.TextArea />
-                                 </Form.Item>
-                              </Form>
-                           </Card>
-                        </div>
-                     )}
-                  </div>
-               ))}
-            </Space>
-
-            {/* Move the Submit button outside the Space component */}
-         </Card>
-
-         <Button type="primary" id="submit" onClick={SubmitAssessment} style={submitButtonStyle}>
-            Submit Assessment
-         </Button>
-      </div>
-   );
+                    ))}
+                </Space>
+                <div style={{ textAlign: 'center', marginTop: '20px' }}>
+                    <Button id="submit" onClick={SubmitAssessment}>Submit Assessment</Button>
+                </div>
+            </Card>
+        </div>
+    )
 }
 
 export default TakeAssess;
