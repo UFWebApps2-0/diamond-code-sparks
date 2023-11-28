08bac24c7fe773eb0f1f0fbaf7df2ca7916a8642
sethbehar <sethbehar@gmail.com>
2023-11-27 22:50:45 -0500

    Cleanup

--- client/src/views/Assessments/CompletedAssessment.jsx
+++ client/src/views/Assessments/CompletedAssessment.jsx
@@ -4,71 +4,71 @@ import { Radio, Space } from 'antd';
 
 
 function CompletedAssessment({ name, answers, classId }) {
-  const [questions, setQuestions] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [selectedOption, setSelectedOption] = useState(); 
+    const [questions, setQuestions] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [selectedOption, setSelectedOption] = useState();
 
 
-  useEffect(() => {
-    const fetchAssessmentQuestions = async (name, id) => {
-      try {
-        const assessmentData = await getClassAssessment(name, id);
-        const assessmentQuestions = assessmentData.data[0].questions;
-        setQuestions(assessmentQuestions);
-        setLoading(false);
-      } catch (error) {
-        console.error('Error fetching assessment:', error.message);
-        setError(error.message);
-        setLoading(false);
-      }
-    };
+    useEffect(() => {
+        const fetchAssessmentQuestions = async (name, id) => {
+            try {
+                const assessmentData = await getClassAssessment(name, id);
+                const assessmentQuestions = assessmentData.data[0].questions;
+                setQuestions(assessmentQuestions);
+                setLoading(false);
+            } catch (error) {
+                console.error('Error fetching assessment:', error.message);
+                setError(error.message);
+                setLoading(false);
+            }
+        };
 
-    fetchAssessmentQuestions(name, classId);
-  }, [name, classId]);
+        fetchAssessmentQuestions(name, classId);
+    }, [name, classId]);
 
-  if (loading) {
-    return <p>Loading...</p>;
-  }
+    if (loading) {
+        return <p>Loading...</p>;
+    }
 
-  if (error) {
-    return <p>Error: {error}</p>;
-  }
+    if (error) {
+        return <p>Error: {error}</p>;
+    }
 
-  return (
-    <div>
-      <h1>{name}</h1>
-      {
-        questions.map((item, index) => (
-          <div key={index}>
-            <h3>Question {index + 1}: {item.question}</h3>
-            <p>{item.type}</p>
-            {item.type === 'Multiple Choice' && (
-              <>
-                <ul>
-                    <Radio.Group value={selectedOption}>
-                        <Space direction="vertical">
-                            {item.option.map((option, optionIndex) => (
-                                <Radio value={optionIndex} key={optionIndex} disabled={answers[index] !== optionIndex}>{option}</Radio>
-                            ))}
-                        </Space>
-                    </Radio.Group>
-                </ul>
-                <br/>
-              </>
-            )}
+    return (
+        <div>
+            <h1>{name}</h1>
+            {
+                questions.map((item, index) => (
+                    <div key={index}>
+                        <h3>Question {index + 1}: {item.question}</h3>
+                        {item.type === 'Multiple Choice' && (
+                            <>
+                                <p>{item.type}</p>
+                                <ul>
+                                    <Radio.Group value={selectedOption}>
+                                        <Space direction="vertical">
+                                            {item.option.map((option, optionIndex) => (
+                                                <Radio value={optionIndex} key={optionIndex} disabled={answers[index] !== optionIndex}>{option}</Radio>
+                                            ))}
+                                        </Space>
+                                    </Radio.Group>
+                                </ul>
+                                <br />
+                            </>
+                        )}
 
-            {item.type === 'Free Response' && (
-                <p>
-                    Student Response: {answers[index]}
-                </p>
-                )}
-            </div>
-            ))
-        }
-    </div>
-  );
-  
+                        {item.type === 'Free Response' && (
+                            <p>
+                                Student Response: {answers[index]}
+                            </p>
+                        )}
+                    </div>
+                ))
+            }
+        </div>
+    );
+
 }
 
 export default CompletedAssessment;
--- client/src/views/Assessments/StudentAssessments.jsx
+++ client/src/views/Assessments/StudentAssessments.jsx
@@ -1,62 +1,61 @@
-import React, {useEffect, useState} from 'react';
-import {Modal,Button,Table} from 'antd';
-import { getClassAssessment,getStudentClassAssessments } from '../../Utils/requests';
+import React, { useEffect, useState } from 'react';
+import { Modal, Button, Table } from 'antd';
+import { getClassAssessment, getStudentClassAssessments } from '../../Utils/requests';
 import CompletedAssessment from './CompletedAssessment';
 //Page to view the assessments a student has taken and to view their completed assessments
-function StudentAssessments({stuId,classId})
-{
-    const [assessments,setAssessments]=useState([]);
-    const [view,setView] = useState(new Array(getStudentClassAssessments(stuId,classId).then((res)=>res.data.length)).fill(false));
-    const columns=[
-        {
-            title: 'Assessment Name',
-            dataIndex: 'name',
-            key: 'name',
-            width: '30%',
-            sorter: {
-              compare: (a, b) => (a.name < b.name ? -1 : 1),
-            },
-          },
-          {
-            title: 'View Assessments',
-            dataIndex: 'assessments',
-            keyIndex: 'assessments',  
-            width: '30%',
+function StudentAssessments({ stuId, classId }) {
+  const [assessments, setAssessments] = useState([]);
+  const [view, setView] = useState(new Array(getStudentClassAssessments(stuId, classId).then((res) => res.data.length)).fill(false));
+  const columns = [
+    {
+      title: 'Assessment Name',
+      dataIndex: 'name',
+      key: 'name',
+      width: '30%',
+      sorter: {
+        compare: (a, b) => (a.name < b.name ? -1 : 1),
+      },
+    },
+    {
+      title: 'View Assessments',
+      dataIndex: 'assessments',
+      keyIndex: 'assessments',
+      width: '30%',
 
-          },
-          {
-            title:'Grade',
-            dataIndex:'grade',
-            keyIndex:'grade',
-            editable:true, 
-            width:'20%'
-          }
-    ]
-
-    function viewWork(i){
-      let temp = [...view];
-      temp[i] = true;
-      setView(temp);
+    },
+    {
+      title: 'Grade',
+      dataIndex: 'grade',
+      keyIndex: 'grade',
+      editable: true,
+      width: '20%'
     }
+  ]
 
-    function leaveWork(i){
-      let temp = [...view];
-      temp[i] = false;
-      setView(temp);
-    }
+  function viewWork(i) {
+    let temp = [...view];
+    temp[i] = true;
+    setView(temp);
+  }
 
-    useEffect(() => {
- 
-      getStudentClassAssessments(stuId,classId).then((res) => {
-        if (res.data) {
-          const data = [];
-          res.data.forEach((assessment,i) => {
+  function leaveWork(i) {
+    let temp = [...view];
+    temp[i] = false;
+    setView(temp);
+  }
 
-            data.push({
-              key: assessment.id,
-              name: assessment.assessmentName,
-              assessments: (
-                <>
+  useEffect(() => {
+
+    getStudentClassAssessments(stuId, classId).then((res) => {
+      if (res.data) {
+        const data = [];
+        res.data.forEach((assessment, i) => {
+
+          data.push({
+            key: assessment.id,
+            name: assessment.assessmentName,
+            assessments: (
+              <>
                 <Button
                   type="primary"
                   onClick={() => {
@@ -66,37 +65,37 @@ function StudentAssessments({stuId,classId})
                   View
                 </Button>
                 <Modal
-                    title={"View Assessment"}
-                    visible={view[i]} // Change 'open' to 'visible'
-                    onCancel={()=>leaveWork(i)}
-                    width={'75vw'}
-                    footer={[
-                      <Button key="ok" type="primary" onClick={()=>leaveWork(i)}>
-                        Cancel
-                      </Button>,
-                    ]}
-                  >
-                    <CompletedAssessment name={assessment.assessmentName} answers={assessment.answers} classId={classId}/>
+                  title={"View Assessment"}
+                  visible={view[i]} // Change 'open' to 'visible'
+                  onCancel={() => leaveWork(i)}
+                  width={'75vw'}
+                  footer={[
+                    <Button key="ok" type="primary" onClick={() => leaveWork(i)}>
+                      Cancel
+                    </Button>,
+                  ]}
+                >
+                  <CompletedAssessment name={assessment.assessmentName} answers={assessment.answers} classId={classId} />
                 </Modal>
               </>
-              ),
-              grade: "N/A",
-            });
+            ),
+            grade: "N/A",
           });
+        });
 
-          setAssessments(data);
-        } else {
-          console.log("error"); 
-        }
-      });
+        setAssessments(data);
+      } else {
+        console.log("error");
+      }
+    });
 
 
-    }, [view]);
-    return (
-        <div>
-            <Table columns={columns} dataSource={assessments} />
-        </div>
-    );
+  }, [view]);
+  return (
+    <div>
+      <Table columns={columns} dataSource={assessments} />
+    </div>
+  );
 }
 
 export default StudentAssessments;
