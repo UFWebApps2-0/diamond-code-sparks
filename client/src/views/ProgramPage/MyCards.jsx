bbad3cdbe97a84094f46b51cfd65e3bb331dec56
Ethan Trosper <ethancoletrosper@gmail.com>
2023-12-05 12:20:16 -0500

    Updating myPrograms page

--- client/src/views/ProgramPage/MyCards.jsx
+++ client/src/views/ProgramPage/MyCards.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import BlocklyPage from '../BlocklyPage/BlocklyPage';
+import { useNavigate } from 'react-router-dom';
 
-const MyCards = ({ programNumber, lessonNumber }) => {
-  const programTitle = `Program ${programNumber} for Lesson ${lessonNumber}`;
+
+const MyCards = ({ programNumber, workspace }) => {
+  const programTitle = `Activity ${programNumber}`;
+  const navigate = useNavigate();
+
+  const handleEditClick = () => {
+    navigate('/sandbox'); // Navigate to BlocklyPage when button is clicked
+  };
+
+  const handleShareClick = () => {
+    navigate('/student');
+  };
+  
 
   return (
     <div className="program">
       <div className="program-title">{programTitle}</div>
-      <button className="edit-button">Edit</button>
-      <button className="share-button">Share</button>
+      <button onClick={handleEditClick} className="edit-button">Edit</button>
+      <button onClick={handleShareClick} className="share-button">Share</button>
     </div>
   );
 };
